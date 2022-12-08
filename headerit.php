a9da34c6a2cb5b1e1bd84d7fbc7939a616ee45a4
techprab <69295906+techprab@users.noreply.github.com>
2022-12-08 23:03:26 +0530

    commit1-by-Prabjot

---
+++ headerit.php
@@ -0,0 +1,29 @@
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+		<img src="img/mbs1.jpg" alt="" width="930" height="70">
+           
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+	 
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Menu</a>
+                    <div class="dropdown-menu fade-down m-0">
+                        <a href="index.html" class="dropdown-item">Home</a>
+                        <a href="testimonial.html" class="dropdown-item">About</a>
+						<a href="team.html" class="dropdown-item">Department</a>
+						<a href="team.html" class="dropdown-item">Contact-Us</a>
+						</div>
+         </div>   
+        
+              <a href="" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Admission<br>Form</a>   
+               </div>     
+				</nav>
+               
+    <!-- Navbar End -->
+	</html>
